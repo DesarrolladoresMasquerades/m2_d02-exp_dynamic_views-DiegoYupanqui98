2c84fd6f2a78051c292142a56b350688ce444fde
Diego Yupanqui <diegoyupanquichavarry@gmail.com>
2022-02-08 15:47:21 +0100

    class done / before layout & partials exercise

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
+const diegoData = require("./diego.json")
 const express = require("express");
 
 require("dotenv").config();
 
 const app = express();
 
+app.set("view engine", "hbs")
+app.set("views", __dirname + "/views")
+
+hbs.registerPartials(__dirname + "/view/partials")
 app.use(express.static("public"));
 
 
 app.get("/home", (req, res, next) =>
-res.sendFile(__dirname + "/views/home.html")
+res.render("home")
 );
 
 app.get("/about", (req, res, next) =>
-res.sendFile(__dirname + "/views/about.html")
+res.render("about")
 );
 
-app.get("/", (req, res, next) => res.send("Hello there!"));
+app.get("/", (req, res, next) => res.render("index",diegoData));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("🏃‍ on port 3000"));
